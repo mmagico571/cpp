1484d1ea730214afef0e21404d93256613dd8a62
mmagico571@gmail.com <mmagico571@gmail.com>
2021-03-31 17:43:55 -0600

    003 Lectura o entrada de datos en C++

---
+++ ats/programas/003.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int numero;
+	cout<<"digite un numero: "<<endl;
+	cin>>numero;
+	cout<<"El numero es: = "<<numero;
+	//comentario una linea
+	/* Comentario
+	lineas
+	multiples */
+}
