e86bca5c2d0e7ac1d53705bb5e32fdad965bb894
mmagico571@gmail.com <mmagico571@gmail.com>
2021-03-31 17:42:02 -0600

    002 Tipos de datos básicos en C++

---
+++ ats/programas/002.cpp
@@ -0,0 +1,13 @@
+//002 Tipos de datos basicos
+#include<iostream>
+using namespace std;
+int main(){
+	int entero=15;
+	cout<<"entero = "<<entero<<endl;
+	float flotante = 3.14;
+	cout<<"flotante = "<<flotante<<endl;
+	double doble = 3.1416;
+	cout<<"doble = "<<doble<<endl;
+	char caracter = 'A';
+	cout<<"caracter = "<<caracter<<"\n";
+}
