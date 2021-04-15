67db60a0b95462ec6e9801c6c82b133d398afe83
mmagico571@gmail.com <mmagico571@gmail.com>
2021-04-15 07:40:57 -0500

    se agrega programa 2.5

---
+++ cpp_para_ingeniera/2/p5.cpp
@@ -0,0 +1,14 @@
+//Programa 2.
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"TIPO DE DATO\t\tBYTES"<<endl;
+	cout<<"int\t\t\t"<<sizeof(int)<<endl;
+	cout<<"short int\t\t"<<sizeof(short int)<<endl;
+	cout<<"long int\t\t"<<sizeof(long int)<<endl;
+	cout<<"float\t\t\t"<<sizeof(float)<<endl;
+	cout<<"double\t\t\t"<<sizeof(double)<<endl;
+	cout<<"long double\t\t"<<sizeof(long double)<<endl;
+	cout<<"char\t\t\t"<<sizeof(char)<<endl;
+	cout<<"bool\t\t\t"<<sizeof(bool)<<endl;
+}
