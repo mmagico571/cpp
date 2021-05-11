595924879f23f62a83f11a83d9388275c83144c9
mmagico571@gmail.com <mmagico571@gmail.com>
2021-05-11 02:30:11 -0500

    020 Ciclos o Bucles La sentencia do while

---
+++ ats/programas/020.cpp
@@ -0,0 +1,19 @@
+/*sentencia do while*/
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int i = 1;
+	do{
+		cout<<i<<endl;
+		i++;	
+	}while(i<=10);
+	
+	i = 10;
+	do{
+		cout<<i<<endl;
+		i--;
+	}while(i>=1);
+	
+	system("pause");
+}
