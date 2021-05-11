5aad1be9028085a7fa83872e1b884d2853dda845
mmagico571@gmail.com <mmagico571@gmail.com>
2021-05-11 00:04:34 -0500

    se agrega programa 2.7a

---
+++ cpp_para_ingeniera/2/p7a.cpp
@@ -0,0 +1,11 @@
+/*Programa 2.7a*/
+#include<iostream>
+using namespace std;
+int main(){
+	double calificacion1, calificacion2, total, promedio;
+	calificacion1 = 85.5;
+	calificacion2 = 97.0;
+	total = calificacion1 + calificacion2;
+	promedio = total / 2;
+	cout<<"El promedio es: "<<promedio;
+}
