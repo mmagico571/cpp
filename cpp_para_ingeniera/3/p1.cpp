e7d6db3bafae6af7cab5d42d826daa6339e2dd12
mmagico571@gmail.com <mmagico571@gmail.com>
2021-05-11 17:44:01 -0500

    se agrega programa 3.1

---
+++ cpp_para_ingeniera/3/p1.cpp
@@ -0,0 +1,12 @@
+/*Programa 3.1*/
+// Calculo del volumen de un cilindro
+#include<iostream>
+using namespace std;
+
+int main(){
+	double radio, altura, volumen;
+	radio = 2.5;
+	altura = 16;
+	volumen = 3.1416 * radio * radio * altura;
+	cout<<"El volumen del cilindor es: "<<volumen;	
+}
