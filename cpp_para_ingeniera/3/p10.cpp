e3b7b5a48ae0b931f5964972f7df8da30ddf41ac
mmagico571@gmail.com <mmagico571@gmail.com>
2021-05-12 18:36:38 -0500

    se agrega programa 3.10

---
+++ cpp_para_ingeniera/3/p10.cpp
@@ -0,0 +1,12 @@
+//Programa 3.10
+#include<iostream>
+using namespace std;
+int main(){
+	double num1, num2, producto;
+	
+	num1=30;
+	num2=0.05;
+	producto=num1*num2;
+	cout<<"30 * 0.05 = "<<producto;
+	
+}
