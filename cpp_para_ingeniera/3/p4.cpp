1f56c1ccbf55f434d3e0249fbb879e2baaadf044
mmagico571@gmail.com <mmagico571@gmail.com>
2021-05-11 21:33:34 -0500

    se agrega programa 4.3

---
+++ cpp_para_ingeniera/3/p4.cpp
@@ -0,0 +1,15 @@
+//Programa 3.4
+#include<iostream>
+using namespace std;
+int main(){
+	int contador = 0;
+	cout<<"valor contador: "<<contador<<endl;
+	contador++;
+	cout<<"valor contador: "<<contador<<endl;
+	contador++;
+	cout<<"valor contador: "<<contador<<endl;
+	contador++;
+	cout<<"valor contador: "<<contador<<endl;
+	contador++;
+	cout<<"valor contador: "<<contador<<endl;
+}
