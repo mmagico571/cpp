e3eaa41abfce0b2cc494de62b0412bc9127f8921
mmagico571@gmail.com <mmagico571@gmail.com>
2021-05-11 21:44:09 -0500

    se agrega programa 3.6

---
+++ cpp_para_ingeniera/3/p6.cpp
@@ -0,0 +1,12 @@
+//Programa 3.6
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main(){
+	cout<<setw(3)<<6<<endl;
+	cout<<setw(3)<<18<<endl;
+	cout<<setw(3)<<124<<endl;
+	cout<<setw(3)<<"---"<<endl;
+	cout<<setw(3)<<(6+18+24)<<endl;
+}
