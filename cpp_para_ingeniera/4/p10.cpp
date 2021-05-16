69fc9511298af557c1e22b7a830246db3a3f4a42
mmagico571@gmail.com <mmagico571@gmail.com>
2021-05-15 19:29:50 -0500

    se agrega programa 4.10

---
+++ cpp_para_ingeniera/4/p10.cpp
@@ -0,0 +1,16 @@
+//programa 4.10
+#include<iostream>
+using namespace std;
+
+int main(){
+	int numero;
+	
+	cout<<"Introdusca un numero : ";
+	cin>>numero;
+	
+	if(numero==5){
+		cout<<"Loteria";
+	}else{
+		cout<<"Loteria";
+	}
+}
