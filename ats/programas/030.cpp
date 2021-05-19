278b63f26c7b22c3121d29ddc3c5a55f8ebc2d74
mmagico571@gmail.com <mmagico571@gmail.com>
2021-05-18 22:50:11 -0500

    030 Arreglos Concepto de Arreglo Unidimensional

---
+++ ats/programas/030.cpp
@@ -0,0 +1,15 @@
+/*Arreglos
+*/
+#include<iostream>
+using namespace std;
+
+int main(){
+		
+		int numero[10];
+		char letras[5];
+		float reales[5];
+		
+		int n[]={1,4,6,7,3};
+		
+		char caracter[]={'a','e','i','o','u'};
+}
