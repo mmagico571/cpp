31a4073c48780dd58318817bda778654fec9d387
mmagico571@gmail.com <mmagico571@gmail.com>
2022-02-05 19:17:11 -0600

    se agrega e2.6.3

--- cpp_para_ingeniera/2/e2.6.3.cpp
+++ cpp_para_ingeniera/2/e2.6.3.cpp
@@ -1,16 +1,29 @@
 //e2.6.3
-/*
+/*Suponga que tiene que escribir un programa para determinar el valor de ergios, da-
+das las relaciones:
+Ergios = fergios * sqrt(lergios)
+Lergios = 2 * π * e^μ
  * */
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 int main(){
 	//salidas
-	
+	float ergios = 0;
 	//Entradas
-	
-	//operaciones
-	
+	float fergios = 0;
+	//constantes
+	float lergios = 0;
+	float pi= 3.1416;
+	float u = 1.672;
+	float e = 2.7818;
+	//entrada de datos
+	cout<<"Ingrese el valor de los fergios: ";
+	cin>>fergios;
+ 	//operaciones
+	lergios = 2 * pi * pow(e,u);
+	ergios = fergios * sqrt(lergios);
 	//salida en pantalla
-	
+	cout<<endl<<ergios;
 }
