87ec3b5a1251acdf4dec21ecfdb77ba68579878b
mmagico571@gmail.com <mmagico571@gmail.com>
2022-02-05 19:21:25 -0600

    se agrega e2.6.4.cpp

--- cpp_para_ingeniera/2/e2.6.4.cpp
+++ cpp_para_ingeniera/2/e2.6.4.cpp
@@ -1,16 +1,16 @@
 //e2.6.4
-/*
+/*Suponga que tiene que escribir un programa para mostrar las siguientes especifica-
+ciones:
+Amplificación de voltaje:	35
+Potencia de salida:			2.5 vatios
+Ancho de banda:				15KHz
  * */
 #include<iostream>
 using namespace std;
 
 int main(){
-	//salidas
-	
-	//Entradas
-	
-	//operaciones
-	
 	//salida en pantalla
-	
+	cout<<"Amplificación de voltaje:		35"<<endl;
+	cout<<"Potencia de salida:			2.5 vatios"<<endl;
+	cout<<"Ancho de banda:				15KHz"<<endl;
 }
