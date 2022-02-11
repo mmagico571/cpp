fb8dd157bacd49693931d6e1a918b3b3c9b1fabf
mmagico571@gmail.com <mmagico571@gmail.com>
2022-02-10 18:30:25 -0600

    se agrega e3.1.1

---
+++ cpp_para_ingeniera/3/e3.1.1.cpp
@@ -0,0 +1,19 @@
+//e3.1.1
+/*
+ Escriba una instrucción de asignación para calcular la circunferencia de un círculo que
+tiene un radio de 3.3 pulgadas. La ecuación para determinar la circunferencia, c, de
+un círculo es c = 2pr, donde r es el radio y p es igual a 3.1416.
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	const double PI=3.14.16;
+	double circunferencia;
+	double radio;
+	
+	circunferencia=2*PI*radio;
+	
+	cout<<"La circunferencia calculada es: "<<circunferencia;
+}
